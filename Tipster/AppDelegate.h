500be00aa807f7efcd19b910748fd0844f6bf673
Federico Baron <federicoagustinbaron@gmail.com>
2020-08-14 17:58:14 -0400

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Federico Baron on 8/14/20.
+//  Copyright © 2020 Federico Baron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tipster/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Tipster
+//
+//  Created by Federico Baron on 8/14/20.
+//  Copyright © 2020 Federico Baron. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
